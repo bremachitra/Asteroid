b0202052a2cab046d20bfeba0a14bea2029d8f5a
bremachitra <bremachitra@gmail.com>
2022-01-30 21:37:46 -0500

    Update Constants.kt
    Key deleted

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY ="sW85g0Wx8DcBeY4BioNbhg6BtJYnC6S76iC0wyQf"
+    const val API_KEY ="Use API Key"
 }
 
